4cdf93819e071dcc354cf40e104a35d2747b2e57
anaferreira <estudioj.ferreira@gmail.com>
2020-11-05 00:08:21 -0300

    first commit

---
+++ js/modules/nav_menu_toggle.js
@@ -0,0 +1,9 @@
+export default function navMenu() {
+  const btnToggle = document.querySelector(".header--toogle-btn");
+  const navMenu = document.querySelector("nav");
+
+  btnToggle.addEventListener("click", () => {
+    btnToggle.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
+}
